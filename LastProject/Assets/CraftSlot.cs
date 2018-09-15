4a7c197c2f3a92ac47be658a04b5dff84fe93f47
최대원 <chdw2438@gmail.com>
2018-09-15 17:52:06 +0900

    조합 시스템 UI 레이아웃 완료

---
+++ LastProject/Assets/CraftSlot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftSlot : MonoBehaviour
+{
+    const string TAG_IMAGE = "CraftItemImage";
+    const string TAG_DESCRIPTION = "CraftItemDescription";
+
+    [SerializeField] UISprite image;
+    [SerializeField] UISprite description;
+
+    private void Start()
+    {
+        UISprite[] sprite = GetComponentsInChildren<UISprite>();
+
+        for (int i = 0; i < sprite.Length; i++)
+        {
+            if(sprite[i].tag == TAG_IMAGE)
+            {
+                image = sprite[i];
+            }
+            else if(sprite[i].tag == TAG_DESCRIPTION)
+            {
+                description = sprite[i];
+            }
+        }
+    }
+
+}
