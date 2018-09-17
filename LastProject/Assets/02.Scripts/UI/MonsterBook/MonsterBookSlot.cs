d8349f20b0d6317896bcea11428d30dad7ff4b71
hajeong-PC\hajeong <hajeongkim1120@gmail.com>
2018-09-18 02:19:19 +0900

    몬스터 도감 조금 작업

---
+++ LastProject/Assets/02.Scripts/UI/MonsterBook/MonsterBookSlot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MonsterBookSlot : MonoBehaviour {
+    public delegate void OnSlotChanged();
+    public OnSlotChanged onSlotChangedCallback;
+
+
+    public UIButton SlotButton;
+
+    MonstersInfo info;
+
+    public void AddMonster(MonstersInfo info)
+    {
+        SlotButton.enabled = true;
+        SlotButton.defaultColor = new Color(0, 0, 0, 1);
+    }
+}
---
+++ LastProject/Assets/02.Scripts/UI/MonsterBook/MonsterBookUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBookUI : MonoBehaviour {
+
+    public GameObject SlotParent;
+
+    private int slotCount = 20;
+    MonsterBookSlot monsterbookslot;
+
+    MonsterBookSlot[] slots;
+
+	void Start () {
+        monsterbookslot.onSlotChangedCallback += UpdateUI;
+        slots = SlotParent.GetComponentsInChildren<MonsterBookSlot>();
+	}
+
+    void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i<slotCount)
+            {
+
+            }
+        }
+    }
+
+}
