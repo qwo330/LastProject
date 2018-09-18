4b6355c2f6e23ebf9adcfabff6f7b3b0e01fcd00
qwo330 <qwd330@naver.com>
2018-09-18 22:14:59 +0900

    craftDB 반영하여 아이템 구조체 생성

--- LastProject/Assets/02.Scripts/Define/ItemLists.cs
+++ LastProject/Assets/02.Scripts/Define/ItemLists.cs
@@ -19,13 +19,26 @@ public enum ItemCodes
     AdamantiumSword = 4,
     MithrilSword = 5,
 
-    Cloth = 11,
-    LeatherArmor = 12,
-    IronArmor = 13,
-    AdamantiumArmor =14,
-    MithrilArmor = 15,
+    FabricHelmet = 11,
+    LeatherHelmet,
+    IronHelmet,
+    AdamantiumHelmet,
+    MithrilHelmet,
 
-    RedPotion = 21,
+    FabricArmor = 21,
+    LeatherArmor,
+    IronArmor,
+    AdamantiumArmor,
+    MithrilArmor,
+
+    FabricShoes = 31,
+    LeatherShoes,
+    IronShoes,
+    AdamantiumShoes,
+    MithrilShoes,
+
+    HalfRedPotion = 41,
+    RedPotion = 42,
 }
 
 public static class ItemLists
@@ -34,6 +47,18 @@ public static class ItemLists
     public static ItemData StoneSword = new ItemData(ItemCodes.StoneSword, ItemTypes.Weapon, 7, 7);
     public static ItemData IronSword = new ItemData(ItemCodes.IronSword, ItemTypes.Weapon, 10, 10);
 
-    public static ItemData RedPotion = new ItemData(ItemCodes.RedPotion, ItemTypes.Eat, 25, 25);
+    public static ItemData FabricHelmet = new ItemData(ItemCodes.FabricHelmet, ItemTypes.Helmet, 5, 5);
+    public static ItemData LeatherHelmet = new ItemData(ItemCodes.LeatherHelmet, ItemTypes.Helmet, 7, 7);
+    public static ItemData IronHelmet = new ItemData(ItemCodes.IronHelmet, ItemTypes.Helmet, 10, 10);
 
+    public static ItemData FabricArmor = new ItemData(ItemCodes.FabricArmor, ItemTypes.Armor, 5, 5);
+    public static ItemData LeatherArmor = new ItemData(ItemCodes.LeatherArmor, ItemTypes.Armor, 7, 7);
+    public static ItemData IronArmor = new ItemData(ItemCodes.IronArmor, ItemTypes.Armor, 10, 10);
+
+    public static ItemData FabricShoes = new ItemData(ItemCodes.FabricShoes, ItemTypes.Shoes, 5, 5);
+    public static ItemData LeatherShoes = new ItemData(ItemCodes.LeatherShoes, ItemTypes.Shoes, 7, 7);
+    public static ItemData IronShoes = new ItemData(ItemCodes.IronShoes, ItemTypes.Shoes, 10, 10);
+
+    public static ItemData HalfRedPotion = new ItemData(ItemCodes.HalfRedPotion, ItemTypes.Eat, 10, 25);
+    public static ItemData RedPotion = new ItemData(ItemCodes.RedPotion, ItemTypes.Eat, 10, 50);
 }
