121b88caf8604b62af07ff1cc111703de332c6a4
박지용 <qwd330@naver.com>
2018-09-19 14:42:43 +0900

    아이템 목록 수정

---
+++ LastProject/Assets/02.Scripts/System/Timer/TimerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerManager : Singleton<TimerManager> {
+
+    public float TimeScale;
+    public float DeltaTime;
+    float startTime;
+    float fixedDeltaTime;
+
+    Queue<GameTimer> TimerPool;
+
+    void init()
+    {
+        TimeScale = 1f;
+        
+    }
+
+    void Update()
+    {
+        DeltaTime = Time.realtimeSinceStartup - startTime;
+        startTime = Time.realtimeSinceStartup;
+
+        Time.fixedDeltaTime = fixedDeltaTime * TimeScale;
+    }
+}
