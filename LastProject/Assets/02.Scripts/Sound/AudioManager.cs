fbc89e607daa9b5a20a0ee13fc9322e621094396
hajeong-PC\hajeong <hajeongkim1120@gmail.com>
2018-09-27 21:31:42 +0900

    오디오매니저 수정

--- LastProject/Assets/02.Scripts/Sound/AudioManager.cs
+++ LastProject/Assets/02.Scripts/Sound/AudioManager.cs
@@ -9,7 +9,12 @@ public enum SoundList
     SFX2,
     SFX3,
 }
-
+public enum BGMLIst
+{
+    BGM1,
+    BGM2,
+    BGM3,
+}
 [System.Serializable]
 public class Sound
 {
@@ -57,14 +62,20 @@ public class Sound
 
 public class AudioManager : Singleton<AudioManager>
 {
-
     [SerializeField]
-    public Sound[] sounds;
+    public Sound[] sounds; // SFX 사운드 이펙트들
+    [SerializeField]
+    public AudioClip[] clips; // BGM 배경음악들
+
+    private AudioSource source;
 
     [SerializeField]
     private Slider volumeSlider;
+    [SerializeField]
+    private Slider BGMSlider;
 
-    //SFX볼륨데이터
+    private WaitForSeconds waitTime = new WaitForSeconds(0.01f);
+  
     public void SetSFXVolume()
     {
         for (int i = 0; i < sounds.Length; i++)
@@ -74,9 +85,21 @@ public class AudioManager : Singleton<AudioManager>
         }
     }
 
+    //SFX볼륨데이터
     public float GetSFXVolume()
     {
         return volumeSlider.value;
+    }
+
+    public void SetBGMVolumn()
+    {
+        source.volume = BGMSlider.value;
+    }
+  
+    //BGM볼륨 데이터
+    public float GetBGMVolume()
+    {
+        return BGMSlider.value;
     }
 
     public static AudioManager instance;
@@ -97,6 +120,7 @@ public class AudioManager : Singleton<AudioManager>
     void Start()
     {
         Initialized();
+        source = GetComponent<AudioSource>();   //BGM은 하나만 존재
     }
 
     public void Initialized()
@@ -109,24 +133,83 @@ public class AudioManager : Singleton<AudioManager>
         }
     }
 
-    public void Play(SoundList name)
+    /// <summary>
+    /// SFX
+    /// </summary>
+    /// <param name="name"></param>
+    public void SFXPlay(SoundList name)
     {
         sounds[(int)name].Play();
     }
 
-    public void Stop(SoundList name)
+    public void SFXStop(SoundList name)
     {
         sounds[(int)name].Stop();
     }
 
-    public void SetLoop(SoundList name)
+    public void SetSFXLoop(SoundList name)
     {
         sounds[(int)name].SetLoop();
     }
 
-    public void SetLoopCancel(SoundList name)
+    public void SetSFXLoopCancel(SoundList name)
     {
         sounds[(int)name].SetLoopCancel();
     }
 
+    /// <summary>
+    ///BGM
+    /// </summary>
+    /// <param name="list"></param>
+    public void BGMPlay(BGMLIst list)
+    {
+        source.volume = 1f;
+        source.clip = clips[(int)list];
+        source.Play();
+    }
+
+    public void PauseBGM()
+    {
+        source.Pause();
+    }
+
+    public void UnpauseBGM()
+    {
+        source.UnPause();
+    }
+
+    public void StopBGM()
+    {
+        source.Stop();
+    }
+
+    public void FadeOutBGMMusic()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeOutMusicCoroutine());
+    }
+
+    IEnumerator FadeOutMusicCoroutine()
+    {
+        for (float i = 1.0f; i >= 0f; i -= 0.01f)
+        {
+            source.volume = i;
+            yield return waitTime;
+        }
+    }
+
+    public void FadeInBGMMusic()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeInMusicCoroutine());
+    }
+    IEnumerator FadeInMusicCoroutine()
+    {
+        for (float i = 0f; i <= 1f; i += 0.01f)
+        {
+            source.volume = i;
+            yield return waitTime;
+        }
+    }
 }
+ 
---
+++ LastProject/Assets/02.Scripts/Sound/SoundTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundTest : MonoBehaviour {
+
+   
+    public void OnClickedSFXButtone()
+    {
+        AudioManager.Instance.SFXPlay(SoundList.SFX1);
+    }
+
+    public void OnDragSFXSlider()
+    {
+        AudioManager.Instance.SetSFXVolume();
+    }
+
+    public void OnClickedBGMButton()
+    {
+        AudioManager.Instance.BGMPlay(BGMLIst.BGM2);
+        AudioManager.Instance.FadeInBGMMusic();
+    }
+
+    public void OnDragBGMSlider()
+    {
+        AudioManager.Instance.SetBGMVolumn();
+    }
+
+}
