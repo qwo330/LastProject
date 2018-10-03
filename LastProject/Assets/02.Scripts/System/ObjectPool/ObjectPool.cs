b36888003a4de9436df02a8629b4faa8c2024f7c
최대원 <chdw2438@gmail.com>
2018-10-03 17:52:19 +0900

    엔트 일부 조정

--- LastProject/Assets/02.Scripts/System/ObjectPool/ObjectPool.cs
+++ LastProject/Assets/02.Scripts/System/ObjectPool/ObjectPool.cs
@@ -10,19 +10,14 @@ public class ObjectPool : Singleton<ObjectPool>
     Player playerCharacter;
     //List<Ent> EntList;
 
-
-
-
-
     public void Init()
     {
         GameObject go = Instantiate(Resources.Load("Prefabs/Archu")) as GameObject;
         playerCharacter = go.GetComponent<Player>();
         playerCharacter.Init();
+        playerCharacter.gameObject.SetActive(false);
         playerCharacter.MovingSpeed = 5; //test용 코드, data 작업 이후 삭제
-        playerCharacter.gameObject.transform.position = new Vector3(40, 1.5f, -11);
+        playerCharacter.gameObject.transform.position = new Vector3(40, 1.5f, -11);//test용 코드, data 작업 이후 삭제
         //playerCharacter.SetStatus();
-
-
     }
 }
