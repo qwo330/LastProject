b2ae5ae3f8c8a1c697868e6507284a7619f9d9a1
박지용 <qwd330@naver.com>
2018-10-04 16:43:12 +0900

    StageManaer 씬 전환 테스트중

---
+++ LastProject/Assets/02.Scripts/System/ManagerCaller.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerCaller : MonoBehaviour {
+
+    private void Awake()
+    {
+        StageManager.Instance.Init();
+    }
+}
